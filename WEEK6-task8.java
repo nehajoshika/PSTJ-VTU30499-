c11d200941ea8993e094d890944336bc608968ab
nehajoshika <nehajoshika27@gmail.com>
2026-09-03 09:22:55 +0530

    Add files via upload

---
+++ WEEK6-task8.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        int[] lps = new int[m];
+
+        // Build LPS array
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+            if (needle.charAt(i) == needle.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        // Search needle in haystack
+        i = 0;
+        int j = 0;
+        while (i < n) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                if (j == m) {
+                    return i - j;
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+output:
+
+Input
+haystack =
+"sadbutsad"
+needle =
+"sad"
+Output
+0
+Expected
+0
